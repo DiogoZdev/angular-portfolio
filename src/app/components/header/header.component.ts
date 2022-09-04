ff120dc3ff8de2b8eeba6c0cc10d509b4618253d
Diogo <diogozang2@gmail.com>
2022-09-04 16:28:13 -0300

    small adjustments

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,13 +9,15 @@ export class HeaderComponent implements OnInit {
 
   public backgrounds: string[] = [
     "../../../assets/backgrounds/city.jpg",
-    "../../../assets/backgrounds/ubuntu.jpg"
+    "../../../assets/backgrounds/ubuntu.jpg",
+    "https://i.picsum.photos/id/902/1200/500.jpg?hmac=ea2N3j3pPTJbs7sZq_2L3zxpnnQAtM6qFTkNp0olUSo",
+    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   ]
 
   constructor() { }
 
   ngOnInit(): void {
-    const num = Math.floor(Math.random()*2);
+    const num = Math.floor(Math.random()*this.backgrounds.length);
 
     let el = document.getElementById("background");
 

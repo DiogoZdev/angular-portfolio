30c37b7eee4f89aa169e5b4feba6d97ceb774b28
Diogo Lara <diogozang2@gmail.com>
2022-09-05 16:37:10 -0300

    blog view + publication adjustments

--- src/app/components/publication/publication.component.ts
+++ src/app/components/publication/publication.component.ts
@@ -3,14 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-publication',
   templateUrl: './publication.component.html',
-  styleUrls: ['./publication.component.scss']
+  styleUrls: ['./publication.component.scss'],
 })
 export class PublicationComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   @Input() image: string = '';
   @Input() title: string = '';
+  @Input() date: string = '';
   @Input() text: string = '';
   @Input() link?: string;
   @Input() tags: string = '';
